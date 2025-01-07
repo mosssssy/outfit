34525748d0cd326aa0e7001cb5aad5de3526ec45
nami <kanakoto.a113@gmail.com>
2025-01-07 18:22:54 +0900

    Add Outfit Color Picker functionality

---
+++ static/js/home.js
@@ -0,0 +1,15 @@
+// Reactをインポートしてください
+import React from 'react';
+
+
+// Homeクラスを定義してください
+class Home extends React.Component {
+  render() {
+    return (
+      <h1>Hello React</h1>
+    );
+  }
+}
+
+// Appクラスをエクスポートしてください
+export default Home;
